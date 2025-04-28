b38260f8139175488b98611b85cf35224814c488
Alina <zaripovaalin@yandex.ru>
2025-04-28 23:16:56 +0300

    four

--- collect_files.sh
+++ collect_files.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+    echo "нужно $0 /путь/к/input_dir /путь/к/output_dir"
+    exit 1
+fi
+
+input_dir = "$1"
+output_dir = "$2"
+
+mkdir -p "$output_dir"
+find "$input_dir" -type f -exec cp -- "{}" "$output_dir" \;
