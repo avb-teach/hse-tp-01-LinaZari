afc2ab6903ba3e0953949996a812ecb4d741fdc9
Alina <zaripovaalin@yandex.ru>
2025-04-29 02:52:19 +0300

    a

--- collect_files.sh
+++ collect_files.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "нужно $0 /путь/к/input_dir /путь/к/output_dir"
-    exit 1
+  exit 1
 fi
 
-input_dir = "$1"
-output_dir = "$2"
+input_dir="$1"
+output_dir="$2"
 
-mkdir -p "$output_dir"
-find "$input_dir" -type f -exec cp -- "{}" "$output_dir" \;
+if [ ! -d "$input_dir" ]; then #проверим существует ли входная директория 
+  exit 1
+fi
+
+mkdir -p "$output_dir" #создание входной директории (если нет) смотрела тут https://github.com/google/deepvariant/blob/r1.8/docs/deepvariant-training-case-study.md?ysclid=ma1pzyk3dq606955235 как правильно создать директорию
+
+find "$input_dir" -mindepth 1 -maxdepth 2 -type f -exec cp -n {} "$output_dir" \!
