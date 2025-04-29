a17ec1efc7f24ae5b5f4ee58cdf94ffb24eb0764
Alina <zaripovaalin@yandex.ru>
2025-04-29 03:41:02 +0300

    try

--- collect_files.sh
+++ collect_files.sh
@@ -7,68 +7,49 @@ fi
 input_dir="$1"
 output_dir="$2"
 
-if [ ! -d "$input_dir" ]; then #проверим существует ли входная директория 
+if [ ! -d "$input_dir" ]; then
   exit 1
 fi
 
-mkdir -p "$output_dir" #создание входной директории (если нет) смотрела тут https://github.com/google/deepvariant/blob/r1.8/docs/deepvariant-training-case-study.md?ysclid=ma1pzyk3dq606955235 как правильно создать директорию
+mkdir -p "$output_dir"
 rm -f "$output_dir"/*
-find "$input_dir" -mindepth 1 -maxdepth 2 -type f -exec cp -n {} "$output_dir" \!
+
+# Копируем только файлы с глубиной ровно 2 
+find "$input_dir" -mindepth 2 -maxdepth 2 -type f -exec cp -n {} "$output_dir" \;
 
 
+declare -A name_counts
 
-
-#обрабаотываем дубл файлы
-
-# идеи брала из https://devhops.ru/linux/bash/scripts/list_files/#small, https://collectingwisdom.com/bash-add-suffix-to-all-files/, https://translated.turbopages.org/proxy_u/en-ru.ru.20e000b7-6810156c-15a9caa3-74722d776562/https/unix.stackexchange.com/questions/468440/find-all-files-with-the-same-name
-
-: '
-POSTFIX="--small"
-
-find "$output_dir" -type f -print0 | awk -v postfix="$POSTFIX" '
-BEGIN { RS="\0" }
-{
-    filename = $NF;
-    if (match(filename, /^(.*)\.[^.]+$/)) {
-        base = substr(filename, RSTART, RLENGTH-1);
-        ext = substr(filename, RSTART + RLENGTH - 1, RLENGTH);
-    } else {
-        base = filename;
-        ext = "";
-    }
-    name_count[base]++;
-    files[base] = files[base] "\0" filename;
-}
-END {
-    for (b in name_count) {
-        if (name_count[b] > 1) {
-            n = split(files[b], arr, "\0");
-            j=1;
-            for (i=1; i<=n; i++) {
-                f=arr[i];
-                newname=b""postfix;
-                if (match(f, /^(.*)(\.[^.]+)$/)) {
-                    ext = substr(f, RSTART, RLENGTH);
-                } else {
-                    ext = "";
-                }
-                newname = b""postfix""ext;
-
-                while (system("[ -e \"" newname "\" ]") == 0) {
-                    newname=b""postfix"(" j ")""" ext;
-                    j++;
-                }
-                system("mv -- \"" f "\" \"" newname "\"");
-            }
-        }
-    }
-}
-'
-'
-
-
-for file in "$output_dir"/*.txt; do
+for file in "$output_dir"/*; do
   if [ -e "$file" ]; then
-    mv "$file" "${file%.txt}_new.txt"
+    filename=$(basename "$file")
+    if [[ $filename == *.txt ]]; then
+      newname="${filename%.txt}_new.txt"
+      mv "$file" "$output_dir/$newname"
+    else
+      base="${filename%.*}"
+      ext="${filename##*.}"
+      
+      if [ -z "$base" ]; then
+        base="$filename"
+        ext=""
+      fi
+      
+      if [ -n "${name_counts[$base]}" ]; then
+        newname="${base}_small${name_counts[$base]}"
+        if [ -n "$ext" ]; then
+          newname="$newname.$ext"
+        fi
+        mv "$file" "$output_dir/$newname"
+        ((name_counts[$base]++))
+      else
+        name_counts[$base]=1
+      fi
+    fi
   fi
 done
+
+
+
+
+#идеи брала https://stackoverflow.com/questions/3129784/bash-script-e-not-detecting-filename-in-a-variable, https://devhops.ru/linux/bash/scripts/list_files/#small, https://translated.turbopages.org/proxy_u/en-ru.ru.20e000b7-6810156c-15a9caa3-74722d776562/https/unix.stackexchange.com/questions/468440/find-all-files-with-the-same-name
